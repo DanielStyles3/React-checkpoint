e9ce5773ef40e1310583e146e8fc39f9341b6bda
DanielStyles3 <danielstyles318@gmail.com>
2023-07-08 15:09:34 +0100

    updated my website

--- src/App.js
+++ src/App.js
@@ -1,55 +1,22 @@
-import React from 'react';
-import { Navbar, Container, Nav, Card } from 'react-bootstrap';
+import React from 'react'
+import "./App.css"
+import Header from './Component/Header'
+import NavMenu from './Component/Navbar'
+import Card from './Component/Cards'
 
-function App() {
+const App = () => {
   return (
-    <>
-      <div className="App">
-        <Navbar bg="light" expand="lg">
-          <Container>
-            <Navbar.Brand href="#">Styles</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#">Home</Nav.Link>
-                <Nav.Link href="#">About</Nav.Link>
-                <Nav.Link href="#">Contact</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        <Container>
-          <h1>Welcome to Styles Website!</h1>
-          <div className="card-group">
-            <Card>
-              <Card.Body>
-                <Card.Title>Card 1</Card.Title>
-                <Card.Text>
-                  This is the first card.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-            <Card>
-              <Card.Body>
-                <Card.Title>Card 2</Card.Title>
-                <Card.Text>
-                  This is the second card.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-            <Card>
-              <Card.Body>
-                <Card.Title>Card 3</Card.Title>
-                <Card.Text>
-                  This is the third card.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </div>
-        </Container>
-      </div>
-    </>
-  );
+    <div>
+      <NavMenu />
+      <Header />
+      <div className='cardsec'>
+        <Card />
+        <Card />
+      <Card />
+      
+    </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
