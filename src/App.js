65c85e1bad8ab462298a2419ecc5242a942f1141
DanielStyles3 <danielstyles318@gmail.com>
2023-07-08 10:47:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Navbar, Container, Nav, Card } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#">Styles</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#">Home</Nav.Link>
+                <Nav.Link href="#">About</Nav.Link>
+                <Nav.Link href="#">Contact</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Container>
+          <h1>Welcome to Styles Website!</h1>
+          <div className="card-group">
+            <Card>
+              <Card.Body>
+                <Card.Title>Card 1</Card.Title>
+                <Card.Text>
+                  This is the first card.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Body>
+                <Card.Title>Card 2</Card.Title>
+                <Card.Text>
+                  This is the second card.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Body>
+                <Card.Title>Card 3</Card.Title>
+                <Card.Text>
+                  This is the third card.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+        </Container>
+      </div>
+    </>
   );
 }
 
